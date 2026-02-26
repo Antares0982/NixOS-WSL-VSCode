487eeb07c32ec31317ad1238898b0607942be9f7
antares <antares@alyr.dev>
2026-02-26 22:11:47 +0800

    Update

--- shell.nix
+++ shell.nix
@@ -10,8 +10,10 @@ pkgs.mkShell {
   shellHook = ''
     export _NIX_INTERPRETER=$(cat ${cc}/nix-support/dynamic-linker)
     export _NIX_RPATH=${cc.cc.lib}/lib
+    export _NIX_LIBZ_RPATH=${pkgs.libz}/lib
     echo _NIX_INTERPRETER=$_NIX_INTERPRETER
     echo _NIX_RPATH=$_NIX_RPATH
-    # patchelf --set-interpreter "$_NIX_INTERPRETER" --set-rpath "$_NIX_RPATH" $_TO_PATCH
+    echo _NIX_LIBZ_RPATH=$_NIX_LIBZ_RPATH
+    # patchelf --set-interpreter "$_NIX_INTERPRETER" --set-rpath "$_NIX_RPATH:$_NIX_LIBZ_RPATH" $_TO_PATCH
   '';
 }
